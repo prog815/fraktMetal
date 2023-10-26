719592f8e6c40d3059435ae81869e2e4aed0ab08
prog <evprog@gmail.com>
2023-10-26 14:26:52 +0500

    рабочий вариант первый

--- app/app.py
+++ app/app.py
@@ -13,9 +13,25 @@ def index():
         file = request.files['file']
         if file:
             img = Image.open(file)
+            
+            img = img.resize((256 , 256), Image.LANCZOS)
             img.save('static/' + file.filename)
-            img = img.rotate(90)
-            img.save('static/pred_' + file.filename)
+            inp_numpy = np.array( img )[None]
+            segmentation_output = model.predict( inp_numpy )[0].argmax(-1)
+            
+            arr = segmentation_output
+
+            rgb_arr = np.zeros((128, 128, 3), dtype=np.uint8)
+
+            dicRGB = {2: (255,0,0), 1: (0,255,0), 0:(0,0,255)}
+
+            for i in range(128):
+                for j in range(128):
+                    rgb_arr[i, j] = dicRGB[arr[i,j]]
+            
+            res_image = Image.fromarray(rgb_arr,'RGB')
+            
+            res_image.save('static/pred_' + file.filename)
             return render_template("index.html",file=file.filename)
     return render_template("index.html")
 
